47bf3106e212b060c8b997bdb90aec0af3db331a
btronquoy <b.tronquoy@gmail.com>
2018-02-27 23:13:59 +0100

    Modification model Recette

--- models/agenda.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require("mongoose");
-
-var AgendaSchema = new mongoose.Schema({
-    date: Date,
-    isactive: { type: Boolean, default: true }
-});
-module.exports = mongoose.model("Agenda", AgendaSchema);
--- models/recette.js
+++ models/recette.js
@@ -1,18 +1,16 @@
 /**
- * 
+ *
  *  @author: Boris Tronquoy
  *  @description: Modèle pour la partie recette
- *  
+ *
 */
 
 var mongoose = require("mongoose");
 
 var RecetteSchema = new mongoose.Schema({
     name: String,
-    datecre: String,
-    status: String,
-    createdby: String,
-    birthdate: String,
-    email: String
+    datecre: Date.now,
+    comments: String,
+    status: Boolean,
 });
 module.exports = mongoose.model("Recette", RecetteSchema);
